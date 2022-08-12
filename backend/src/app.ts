8434dc7fe0e46d46e8ec38ee66aef213183e9aca
ergunirem <cikrikciogluirem@gmail.com>
2022-08-12 18:56:27 +0200

    video from local path decoded, trimmed, output video createdwith ffmpeg with hardcoded input

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import ffmpeg from "fluent-ffmpeg";
+import { resolve } from "path";
 
 dotenv.config();
 if (!process.env.PORT) {
@@ -12,11 +14,43 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json()); //middleware to parse request body
 
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+
+async function cutVideo() {
+    console.log('Start cutting the video...');
+
+    await new Promise<void>((resolve, reject) => {
+        ffmpeg('/Users/ergunirem/Desktop/Photos/Samsung Tel/WhatsApp/Media/WhatsApp Video/VID-20191217-WA0023.mp4')
+            .output('../output/Video.mp4')
+            .setStartTime(3)
+            .setDuration(1)
+            // .withVideoCodec('copy')
+            // .withAudioCodec('copy')
+            .on('end', function(err) {
+                if(!err) {
+                    console.log('Conversion done!');
+                    resolve();
+                }
+            })
+            .on('error', function (err) {
+                console.log('error: ', err);
+                reject(err);
+              })
+            .run();
+    })
+};
+
 app.post('/', (req: Request, res: Response) => {
     console.log(req.body);
+    //decode mp4 video with FFmpeg
+    console.log('Decoding from local and trimming...')
+    cutVideo();
     res.send(req.body);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running at https://localhost:${PORT}`);
-})
+});
