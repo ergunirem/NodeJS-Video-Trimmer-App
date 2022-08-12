ce6d87569445aa164142f2f6b1941d0b5817f66f
ergunirem <cikrikciogluirem@gmail.com>
2022-08-12 22:26:05 +0200

    video downloaded from URL with axios get request and fs's write stream

---
+++ backend/src/services/download.service.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import * as fs from 'fs';
+import * as path from 'path';
+
+export async function downloadVideoFromURL(videoUrl: string, downloadFolder: string) {
+
+    const fileName = path.basename(videoUrl); //used to get the file name from the URL
+    const localFilePath = path.resolve(__dirname, downloadFolder, fileName); // The path of the downloaded file on our machine
+    console.log(`localFilePath ${localFilePath}`);
+    try {
+        const res = await axios({
+            method: 'GET',
+            url: videoUrl,
+            responseType: 'stream',
+          });
+
+        const w = res.data.pipe(fs.createWriteStream(localFilePath));
+        w.on('finish', () => {
+            console.log('Successfully downloaded file!');
+        });
+    } catch (error: any) {
+        throw new Error(error);
+    }
+};
