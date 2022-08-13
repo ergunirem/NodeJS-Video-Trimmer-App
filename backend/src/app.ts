8ff4f60a52bdbfb47cf77a4f8ef6e5e41b328edb
ergunirem <cikrikciogluirem@gmail.com>
2022-08-13 10:58:44 +0200

    error handling logic according to async express app added with next() function

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express from "express";
+import type { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { cutVideo } from "./services/trim.service"
+import { cutVideo } from "./services/trim.service";
+import { downloadVideoFromURL } from "./services/download.service";
+import { errorHandler } from './middlewares/error.handler'
 
 dotenv.config();
 if (!process.env.PORT) {
@@ -13,14 +16,28 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json()); //middleware to parse request body
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
+app.post('/', async (
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+    try {
+        console.log(req.body);
 
-    //decode mp4 video with FFmpeg
-    cutVideo();
-    res.send(req.body);
+        //download video from URL
+        await downloadVideoFromURL('https://www.kindacode.com/wp-content/uploads/2021/01/example.mp4', 'input');
+
+        //decode mp4 video with FFmpeg
+        cutVideo();
+
+        //TODO Responses should be in JSON format
+        res.send(req.body);
+    } catch(error: any) {
+        next(error);
+    }
 });
 
+app.use(errorHandler);
 app.listen(PORT, () => {
     console.log(`Server is running at https://localhost:${PORT}`);
 });
+
---
+++ backend/src/middlewares/error.handler.ts
@@ -0,0 +1,18 @@
+import type { Request, Response, NextFunction } from "express";
+
+/**
+ * Custom error handler to standardize error objects
+ * returned to the client
+ *
+ * @param err Error caught by Express.js
+ * @param req Request object provided by Express
+ * @param res Response object provided by Express
+ * @param next NextFunction function provided by Express
+ */
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+
+    res.header("Content-Type", 'application/json'); //TODO Responses from the endpoint should be in JSON format.
+
+    const status = err.status || 400;
+    res.status(status).send(err.message);
+};
