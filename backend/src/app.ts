37b23082342b26a95251ef1c38a15dc1eb6644f7
ergunirem <cikrikciogluirem@gmail.com>
2022-08-13 14:42:19 +0200

    mongoDB connection created with mongoose

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 import { cutVideo } from "./services/trim.service";
 import { downloadVideoFromURL } from "./services/download.service";
 import { errorHandler } from './middlewares/error.handler'
@@ -15,6 +16,27 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 const app: Express = express();
 app.use(cors());
 app.use(express.json()); //middleware to parse request body
+
+// Build the connection string with type guard for string && Create the database connection
+const MONGODB_URI: string = process.env.MONGODB_URI !== undefined ? process.env.MONGODB_URI : '';
+mongoose.connect(MONGODB_URI)
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open');
+});
+mongoose.connection.on('error',function (err) {
+    console.log(`Mongoose default connection error: ${err}`);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+    });
+});
 
 app.post('/', async (
     req: Request,
