51a6c60a163ea2ed6269b8118ce13b0772ab0fa4
ergunirem <cikrikciogluirem@gmail.com>
2022-08-13 22:03:52 +0200

    local file path rearranged

--- backend/src/services/download.service.ts
+++ backend/src/services/download.service.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 export async function downloadVideoFromURL(videoUrl: string, downloadFolder: string) {
 
     const fileName = path.basename(videoUrl); //used to get the file name from the URL
-    const localFilePath = path.resolve(__dirname, downloadFolder, fileName); // The path of the downloaded file on our machine
+    const localFilePath = '../input/' + fileName;// The path of the downloaded file on our machine
     console.log(`localFilePath ${localFilePath}`);
     try {
         const res = await axios({
